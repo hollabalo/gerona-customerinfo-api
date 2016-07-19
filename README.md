3cf29a616202703c0eac5ec9ccf8149dc188540d
JM Gerona <jdgerona@smartemoneyinc.com>
2016-07-19 20:02:54 +0800

    Added codes for service layer.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Customer Info API
 for Recruit IT process
 
-[![Build Status](https://travis-ci.org/scias23/gerona-recruitit-customerinfo.svg?branch=master)](https://travis-ci.org/scias23/gerona-recruitit-customerinfo)
+[![Build Status](https://travis-ci.org/scias23/gerona-customerinfo-api.svg?branch=master)](https://travis-ci.org/scias23/gerona-customerinfo-api)
--- src/main/java/com/recruitit/customerinfo/service/CustomerInfoService.java
+++ src/main/java/com/recruitit/customerinfo/service/CustomerInfoService.java
@@ -3,11 +3,14 @@ package com.recruitit.customerinfo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.recruitit.customer.types.Address;
+import com.recruitit.customer.types.CustomerInfo;
 import com.recruitit.customer.types.CustomerInfoCreateAndUpdateResponse;
 import com.recruitit.customer.types.CustomerInfoCreateRequest;
 import com.recruitit.customer.types.CustomerInfoDeleteResponse;
 import com.recruitit.customer.types.CustomerInfoResponse;
 import com.recruitit.customer.types.CustomerInfoUpdateRequest;
+import com.recruitit.customer.types.Name;
 import com.recruitit.customerinfo.exception.CustomerNotFoundException;
 import com.recruitit.customerinfo.model.Customer;
 import com.recruitit.customerinfo.repository.CustomerRepository;
@@ -15,23 +18,86 @@ import com.recruitit.customerinfo.repository.CustomerRepository;
 @Service
 public class CustomerInfoService {
 
+	private static final int OPERATION_SUCCESSFUL = 0;
+	
 	@Autowired
 	private CustomerRepository repository;
 	
 	public CustomerInfoResponse findById(String id) throws CustomerNotFoundException {
-		return null;
+		Customer customer = customerRecordIntegrityCheck(id);
+		
+		CustomerInfoResponse response = new CustomerInfoResponse();
+		response.setId(customer.getId());
+		response.setAddress(new Address(customer.getHouseBrgy(), customer.getCity(), customer.getProvince()));
+		response.setCustomerName(new Name(customer.getLastName(), customer.getFirstName(), customer.getMiddleName()));
+		response.setTelephone(customer.getTelephone());
+		
+		return response;
 	}
 	
 	public CustomerInfoCreateAndUpdateResponse create(CustomerInfoCreateRequest request) {
-		return null;
+		Customer customer = new Customer();
+		customer.setLastName(request.getCustomerName().getLastName());
+		customer.setFirstName(request.getCustomerName().getFirstName());
+		customer.setMiddleName(request.getCustomerName().getMiddleName());
+		customer.setHouseBrgy(request.getAddress().getHouseBrgy());
+		customer.setCity(request.getAddress().getCity());
+		customer.setProvince(request.getAddress().getProvince());
+		customer.setTelephone(request.getTelephone());
+		
+		Customer savedInfo = repository.save(customer);
+		
+		CustomerInfo customerInfo = new CustomerInfo();
+		customerInfo.setId(savedInfo.getId());
+		customerInfo.setCustomerName(new Name(savedInfo.getLastName(), savedInfo.getFirstName(), savedInfo.getMiddleName()));
+		customerInfo.setAddress(new Address(savedInfo.getHouseBrgy(), savedInfo.getCity(), savedInfo.getProvince()));
+		customerInfo.setTelephone(savedInfo.getTelephone());
+		
+		CustomerInfoCreateAndUpdateResponse response = new CustomerInfoCreateAndUpdateResponse();
+		response.setCustomerInfo(customerInfo);
+		response.setCode(OPERATION_SUCCESSFUL);
+		response.setMessage("You have successfully created a customer");
+		
+		return response;
 	}
 	
 	public CustomerInfoCreateAndUpdateResponse update(String id, CustomerInfoUpdateRequest request) throws CustomerNotFoundException {
-		return null;
+		Customer customer = customerRecordIntegrityCheck(id);
+		
+		customer.setLastName(request.getCustomerName().getLastName());
+		customer.setFirstName(request.getCustomerName().getFirstName());
+		customer.setMiddleName(request.getCustomerName().getMiddleName());
+		customer.setHouseBrgy(request.getAddress().getHouseBrgy());
+		customer.setCity(request.getAddress().getCity());
+		customer.setProvince(request.getAddress().getProvince());
+		customer.setTelephone(request.getTelephone());
+		
+		Customer savedInfo = repository.save(customer);
+		
+		CustomerInfo customerInfo = new CustomerInfo();
+		customerInfo.setId(savedInfo.getId());
+		customerInfo.setCustomerName(new Name(savedInfo.getLastName(), savedInfo.getFirstName(), savedInfo.getMiddleName()));
+		customerInfo.setAddress(new Address(savedInfo.getHouseBrgy(), savedInfo.getCity(), savedInfo.getProvince()));
+		customerInfo.setTelephone(savedInfo.getTelephone());
+		
+		CustomerInfoCreateAndUpdateResponse response = new CustomerInfoCreateAndUpdateResponse();
+		response.setCustomerInfo(customerInfo);
+		response.setCode(OPERATION_SUCCESSFUL);
+		response.setMessage("You have successfully updated a customer");
+		
+		return response;
 	}
 	
 	public CustomerInfoDeleteResponse delete(String id) throws CustomerNotFoundException {
-		return null;
+		Customer customer = customerRecordIntegrityCheck(id);
+		
+		repository.delete(customer);
+		
+		CustomerInfoDeleteResponse response = new CustomerInfoDeleteResponse();
+		response.setCode(OPERATION_SUCCESSFUL);
+		response.setMessage("You have successfully deleted a customer.");
+		
+		return response;
 	}
 	
 	private Customer customerRecordIntegrityCheck(String id) throws CustomerNotFoundException {
